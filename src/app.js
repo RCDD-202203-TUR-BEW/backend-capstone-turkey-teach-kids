34d550442e0baf33509a4cbea43ad3b9b3773e31
yamanrajab <yamanrajab@hotmail.com>
2022-07-24 18:09:46 +0300

    Setup Mongodb-Atlas and mongooose connection Fixes #1

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 require('express-async-errors');
+require("dotenv").config();
 const routes = require('./routes');
 const errorHandler = require('./middlewares/error');
-
+const connectToMongo = require("./db/connection");
 const app = express();
 
 const port = process.env.PORT || 3000;
