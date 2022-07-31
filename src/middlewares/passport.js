183ee0363bafd46acbde27a0865438ad5ece25b5
yamanrajab <yamanrajab@hotmail.com>
2022-08-01 02:02:09 +0300

    Implement google authentication

---
+++ src/middlewares/passport.js
@@ -0,0 +1,43 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const Volunteer = require('../models/volunteer');
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GAPP_CLIENT_ID,
+      clientSecret: process.env.GAPP_CLIENT_SECRET,
+      callbackURL: 'http://localhost:3000/api/auth/google/callback',
+    },
+    async (accessToken, refreshToken, profile, callBack) => {
+      const currentUser = await Volunteer.findOne({ providerId: profile.id });
+
+      if (currentUser) {
+        callBack(null, currentUser);
+      } else {
+        const newVolunteer = new Volunteer({
+          email: profile.json.email,
+          providerId: profile.id,
+          firstName: profile.json.given_name,
+          lastName: profile.json.family_name,
+          provider: profile.provider,
+        });
+
+        newVolunteer.save();
+        callBack(null, newVolunteer);
+      }
+    }
+  )
+);
+passport.serializeUser((user, cb) => {
+  cb(null, user.id);
+});
+
+passport.deserializeUser((id, cb) => {
+  Volunteer.findById(id).then((user) => {
+    cb(null, user);
+  });
+});
+// Implement google authentication
+// Fixes #10
