cbd2ec7fa38e538e5420423add814e69ab752b6d
Muslim Omar <mislimumer@gmail.com>
2022-08-01 16:00:55 +0300

    feat: integrate swagger

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 require('express-async-errors');
+const swaggerUi = require('swagger-ui-express');
 const routes = require('./routes');
 const errorHandler = require('./middlewares/error');
 const logger = require('./utils/logger');
+const swaggerDocument = require('../swagger.json');
+
 require('dotenv').config();
 
 const app = express();
@@ -12,6 +15,8 @@ const port = process.env.NODE_LOCAL_PORT || 3000;
 app.use('/api', routes);
 
 app.use(errorHandler);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   logger.info(`listening on port ${port}`);
