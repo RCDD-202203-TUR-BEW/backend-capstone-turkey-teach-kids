14aad5bd6fec76d6b009933b70b6eb6128416514
yamanrajab <yamanrajab@hotmail.com>
2022-08-02 04:02:45 +0300

    Implement google authentication Fixes #10

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 require('express-async-errors');
 require('dotenv').config();
+const swaggerUi = require('swagger-ui-express');
 const connectToMongo = require('./db/connection');
 const routes = require('./routes');
 const errorHandler = require('./middlewares/error');
 const logger = require('./utils/logger');
 require('dotenv').config();
+const swaggerDocument = require('../swagger.json');
 
 const app = express();
 
@@ -15,6 +17,7 @@ app.use('/api', routes);
 
 app.use(errorHandler);
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(port, () => {
   logger.info(`listening on port ${port}`);
   connectToMongo();
