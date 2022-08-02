3a9f20aa69511a88c2d16aeb376b2b08c0b7557c
Abdul-Hafiz-Mhammadah <hafeez_96@hotmail.com>
2022-08-02 17:00:00 +0300

    creating basic files and folders

---
+++ src/routes/profile.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const ErrorResponse = require('../utils/errorResponse');
+
+const profileController = require('../controllers/profile');
+
+router.get('/profile', profileController.getProfile);
+
+module.exports = router;
