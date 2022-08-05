3eec8ea221eeb3b38dd1f95213885255ac4232e1
yamanrajab <yamanrajab@hotmail.com>
2022-08-05 11:34:32 +0300

    Implement GET /ngos Fixes #45

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const usersRoutes = require('./users');
 const authRoutes = require('./auth');
+const ngoRoutes = require('./ngo');
 
 router.use('/users', usersRoutes);
 router.use('/auth', authRoutes);
+router.use('/ngos', ngoRoutes);
 
 module.exports = router;
