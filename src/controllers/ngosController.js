936ee2df1456047b3a733c1123d2c8aa9606ffef
yamanrajab <yamanrajab@hotmail.com>
2022-08-10 12:38:05 +0300

    add Feature GET/ngos/:id with test and doc

--- src/controllers/ngosController.js
+++ src/controllers/ngosController.js
@@ -1,10 +1,13 @@
 const Ngo = require('../models/ngo');
 const ErrorResponse = require('../utils/errorResponse');
 
-exports.getNgos = async (_, res, next) => {
-  const ngos = await Ngo.find();
-  if (ngos.length === 0) {
-    return next(new ErrorResponse('Ther are no listed Ngos yet', 404));
+exports.getNgo = async (req, res, next) => {
+  const ngo = await Ngo.findById(req.params.id);
+
+  if (ngo.length === 0) {
+    return next(
+      new ErrorResponse('This Id dose not match any registered NGO ', 404)
+    );
   }
-  return res.status(200).json({ success: true, data: ngos });
+  return res.status(200).json({ success: true, data: ngo });
 };
--- src/routes/ngo.js
+++ src/routes/ngo.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 const ngosControllers = require('../controllers/ngosController');
 
-router.get('/', ngosControllers.getNgos);
+router.get('/id', ngosControllers.getNgo);
 
 module.exports = router;
