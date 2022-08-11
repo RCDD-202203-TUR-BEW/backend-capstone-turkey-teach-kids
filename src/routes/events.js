38c455f6e3bc059ad37690175011c2383593c5e8
Muslim Omar <mislimumer@gmail.com>
2022-08-11 13:06:02 +0300

    Revert "add PATCH /api/events/:id"
    This reverts commit 4ce74d5cef1c620c06fec712d514f6ed86f9919f.

--- src/routes/events.js
+++ src/routes/events.js
@@ -8,6 +8,4 @@ router.get('/:id', eventsControllers.getEvent);
 
 router.get('/:id/related-events', eventsControllers.getRelatedEvents);
 
-router.patch('/:id', eventsControllers.updateEvent);
-
 module.exports = router;
