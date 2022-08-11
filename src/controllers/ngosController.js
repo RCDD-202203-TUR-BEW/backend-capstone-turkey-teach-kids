22ab14bad673d48d6911878348e646cb84ff21e1
yamanrajab <yamanrajab@hotmail.com>
2022-08-11 14:37:25 +0300

    add feature Get/ngos/:id/events with doc and test

--- src/controllers/ngosController.js
+++ src/controllers/ngosController.js
@@ -16,3 +16,14 @@ exports.getNgos = async (req, res, next) => {
   const ngos = await Ngo.find();
   return res.status(200).json({ success: true, data: ngos });
 };
+exports.getNgoEvents = async (req, res, next) => {
+  const ngo = await Ngo.findById(req.params.id);
+
+  if (!ngo) {
+    return next(
+      new ErrorResponse('This Id dose not match any registered NGO ', 404)
+    );
+  }
+  const ngoEvents = ngo.publishedEvents;
+  return res.status(200).json({ success: true, data: ngoEvents });
+};
--- src/routes/ngo.js
+++ src/routes/ngo.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 const ngosControllers = require('../controllers/ngosController');
 
-router.get('/id', ngosControllers.getNgo);
+router.get('/:id', ngosControllers.getNgo);
+
+router.get('/', ngosControllers.getNgos);
 
 module.exports = router;
