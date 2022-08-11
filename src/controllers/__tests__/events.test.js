3fc0a7aa4566c490cfc0a228e7c85ce7919c8c3b
BerkAkipek <berk.akipek.96@gmail.com>
2022-08-11 20:52:07 +0300

    add PATCH /api/events/:id

--- src/controllers/__tests__/events.test.js
+++ src/controllers/__tests__/events.test.js
@@ -150,4 +150,26 @@ describe("Testing events for routes doesn't require auth controls", () => {
         return events;
       });
   });
+
+  it('PATCH /api/events/:id should update an existing event', (done) => {
+    request(app)
+      .patch(`/api/events/1/`)
+      .send(events[0])
+      .expect('Content-Type', /json/)
+      .expect(200, (err, res) => {
+        if (err) {
+          done();
+          return err;
+        }
+        if (!res.body) {
+          expect(res.statusCode).toBe(404);
+          done();
+          return res.json('No event found');
+        }
+        expect(res.body.data).toEqual(events[0]);
+        expect(typeof res.body).toEqual('object');
+        done();
+        return events;
+      });
+  });
 });
