cec53ea7903b343e5970541b585f6adeb1e7a626
yamanrajab <yamanrajab@hotmail.com>
2022-08-12 12:55:28 +0300

    implementing events/:id/pending-applicants/:userId/approve

--- src/controllers/events.js
+++ src/controllers/events.js
@@ -1,5 +1,5 @@
 const Event = require('../models/event');
-const { Ngo } = require('../models/user');
+const { Ngo, User, Volunteer } = require('../models/user');
 const ErrorResponse = require('../utils/errorResponse');
 
 exports.getEvents = async (req, res) => {
@@ -28,4 +28,30 @@ exports.getRelatedEvents = async (req, res, next) => {
     .select('-pendingApplicants -approvedApplicants -declinedApplicants')
     .populate('ngo', 'name');
   return res.status(200).json({ success: true, data: relatedEvents });
+};
+
+exports.approveApplicant = async (req, res, next) => {
+  const event = await Event.findById(req.params.id);
+  if (!event) {
+    return next(new ErrorResponse('No event found', 404));
+  }
+  const eventOwner = event.ngo._id;
+  if (eventOwner !== User._id) {
+    return next(
+      new ErrorResponse(
+        'you can only approve or decline applicants of your event',
+        400
+      )
+    );
+  }
+  const applicant = await Volunteer.findById(req.params.userId);
+  if (!applicant) {
+    return next(new ErrorResponse('No user found', 404));
+  }
+  if (event.pendingApplicants.inludes(applicant) === false) {
+    return next(
+      new ErrorResponse('This volunteer did not apply for this event', 404)
+    );
+  }
+  event.approvedApplicants.push(applicant);
 };
--- src/routes/events.js
+++ src/routes/events.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const { isAuth, isNgo } = require('../middlewares/auth');
 const eventsControllers = require('../controllers/events');
 //  TODO: AUTH middleware will be added .
 router.get('/', eventsControllers.getEvents);
@@ -7,5 +7,12 @@ router.get('/', eventsControllers.getEvents);
 router.get('/:id', eventsControllers.getEvent);
 
 router.get('/:id/related-events', eventsControllers.getRelatedEvents);
+
+router.post(
+  '/:id/pending-applicants/:userId/approve',
+  isAuth,
+  isNgo,
+  eventsControllers.approveApplicant
+);
 
 module.exports = router;
