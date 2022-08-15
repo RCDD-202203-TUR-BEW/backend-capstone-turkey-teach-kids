81785e5b03f28539da66cb46183facde39e7ba50
sobhan <somayahshams1995@gmail.com>
2022-08-15 08:55:06 +0300

    Implement `GET /volunteers/:id` Fixes #13

--- src/routes/volunteers.js
+++ src/routes/volunteers.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const { isAuth } = require('../middlewares/auth');
 // const { request } = require('chai');
 
+const ErrorResponse = require('../utils/errorResponse');
+
 const volunteersController = require('../controllers/volunteers');
 
 // router.get('/', volunteersController.getVolunteers);
