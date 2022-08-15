1c4cd4ea8efd19408f8a9b874ed7b7d41c078728
BerkAkipek <berk.akipek.96@gmail.com>
2022-08-15 10:30:32 +0300

    fixes issue24

--- src/controllers/events.js
+++ src/controllers/events.js
@@ -1,5 +1,6 @@
+const { default: mongoose } = require('mongoose');
 const Event = require('../models/event');
-const { Ngo } = require('../models/user');
+const { Volunteer } = require('../models/user');
 const ErrorResponse = require('../utils/errorResponse');
 
 exports.getEvents = async (req, res) => {
@@ -28,4 +29,29 @@ exports.getRelatedEvents = async (req, res, next) => {
     .select('-pendingApplicants -approvedApplicants -declinedApplicants')
     .populate('ngo', 'name');
   return res.status(200).json({ success: true, data: relatedEvents });
+};
+
+exports.declineApplicant = async (req, res, next) => {
+  const event = await Event.findById(req.params.id);
+  if (!event) {
+    return next(new ErrorResponse('No event found', 404));
+  }
+  const applicant = await Volunteer.findById(req.params.userId);
+  const applicantIndex = event.pendingApplicants.indexOf(applicant._id);
+  if (!applicant) {
+    return next(new ErrorResponse('No user found', 404));
+  }
+  if (applicantIndex === -1) {
+    return next(new ErrorResponse('User not found in pending applicants', 404));
+  }
+  await Event.findOneAndUpdate(
+    { _id: mongoose.Types.ObjectId(req.params.id) },
+    { $push: { declinedApplicants: applicant._id } }
+  );
+  await Event.findOneAndUpdate(
+    { _id: mongoose.Types.ObjectId(req.params.id) },
+    { $pull: { pendingApplicants: applicant._id } }
+  );
+  const updatedEvent = await Event.findById(req.params.id);
+  return res.status(200).json({ success: true, data: updatedEvent });
 };
--- src/routes/events.js
+++ src/routes/events.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const { isAuth } = require('../middlewares/auth');
 const eventsControllers = require('../controllers/events');
 //  TODO: AUTH middleware will be added .
 router.get('/', eventsControllers.getEvents);
@@ -7,5 +7,11 @@ router.get('/', eventsControllers.getEvents);
 router.get('/:id', eventsControllers.getEvent);
 
 router.get('/:id/related-events', eventsControllers.getRelatedEvents);
+
+router.post(
+  '/:id/pending-applicants/:userId/decline',
+  isAuth,
+  eventsControllers.declineApplicant
+);
 
 module.exports = router;
