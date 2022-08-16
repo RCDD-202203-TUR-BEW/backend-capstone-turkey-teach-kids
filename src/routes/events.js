765a4865b9f046d2e1f34d7f3b133860ea3d285d
BerkAkipek <berk.akipek.96@gmail.com>
2022-08-16 13:22:51 +0300

    add middlewares for PATCH /api/events/:id

--- src/routes/events.js
+++ src/routes/events.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const { isAuth, isNgo } = require('../middlewares/auth');
 const eventsControllers = require('../controllers/events');
 //  TODO: AUTH middleware will be added .
 router.get('/', eventsControllers.getEvents);
@@ -8,6 +8,6 @@ router.get('/:id', eventsControllers.getEvent);
 
 router.get('/:id/related-events', eventsControllers.getRelatedEvents);
 
-router.patch('/:id', eventsControllers.updateEvent);
+router.patch('/:id', isAuth, isNgo, eventsControllers.updateEvent);
 
 module.exports = router;
